6913aa7363f6d528a6ebab1e33c9908d741d7796
KikiIskandar12200500 <kikiiskandar2001@gmail.com>
2021-09-20 17:50:25 +0700

    latihan contoh2 dan file projek

---
+++ bcit-ci-CodeIgniter-b73eb19/application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+{
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya KikiIskandar 
+            saya mahasiswa UBSI
+            Jurusan Sistem Informasi Semester 3 
+            Tempat Tinggal saya di Lwanggintung Sukasari Bogor
+            Saya senang olahraga dan menonton film"; 
+    } 
+}
